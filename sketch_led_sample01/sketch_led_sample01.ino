b6e47f1601f72c01847e08545d3430c22639f012
ユウ <sukesuke0718@gmail.com>
2019-07-27 23:10:21 +0900

    点灯

--- sketch_led_sample01/sketch_led_sample01.ino
+++ sketch_led_sample01/sketch_led_sample01.ino
@@ -1,12 +1,20 @@
 int LED_SOCKET  = 10;
+int INTERVAL  = 1000;     // 1秒
 
+// 初めに実行される関数
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_SOCKET, OUTPUT);
 
 }
 
+// 繰り返し実行される関数
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_SOCKET, HIGH);
+  
+  delay(INTERVAL);
+  digitalWrite(LED_SOCKET, LOW);
+
+  delay(INTERVAL);
 }
