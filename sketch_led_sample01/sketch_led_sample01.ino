a464127730bbc210ea5cfaf5863fcd39eda929c6
ユウ <sukesuke0718@gmail.com>
2019-07-27 23:01:33 +0900

    new create

---
+++ sketch_led_sample01/sketch_led_sample01.ino
@@ -0,0 +1,12 @@
+int LED_SOCKET  = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_SOCKET, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_SOCKET, HIGH);
+}
